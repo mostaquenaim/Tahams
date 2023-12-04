0471cacd3ba89f7bbd5013c2f8b65d004e16be48
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2023-12-04 16:45:16 +0600

    deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AdminModule } from './Admin/Modules/adminModule.module';
 import { CustomerModule } from './Customer/Modules/customerModule.module';
 import { GlobalModule } from './Global/Modules/global.module';
 import { EmployeeModule } from './Employee/Modules/employeeModule.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -21,9 +23,13 @@ import { EmployeeModule } from './Employee/Modules/employeeModule.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([]),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '../public'), // added ../ to get one folder back
+      serveRoot: '/public/', //last slash was important
+    }),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
