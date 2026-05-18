3e596cdbc8df298bcd53dfcba2846eb74a062ac9
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2026-05-18 20:15:59 +0600

    removed: session

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
-  app.use(
-    session({
-      secret: configService.get('SESSION_SECRET'),
-      resave: true,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: parseInt(configService.get('SESSION_MAX_AGE')),
-      },
-    }),
-  );
   app.enableCors({
     origin: [
       'http://127.0.0.1:8000',
@@ -26,9 +13,12 @@ async function bootstrap() {
       'https://www.tahamsbd.com',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
+
   app.setGlobalPrefix('api');
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
